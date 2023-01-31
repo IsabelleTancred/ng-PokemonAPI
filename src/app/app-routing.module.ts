73e12c038c67f37c71dcea542415d2e540e40b45
IsabelleTancred <isabelleetancred@gmail.com>
2023-01-31 22:16:15 +0100

    feat: crated login, trainer and catalogue pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginPage } from "./pages/login/login.page";
+import { PokemonCataloguePage } from "./pages/pokemon-catalogue/pokemon-catalogue.page";
+import { TrainerPage } from "./pages/trainer/trainer.page";
+
+const routes: Routes = [
+    { path: "", 
+    component: LoginPage},
+    { path: "pokemons",
+    component: PokemonCataloguePage},
+    { path: "trainer",
+    component: TrainerPage}
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule { 
+
+}
