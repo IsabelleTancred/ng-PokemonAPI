dccee18e31993f1974b3e5b1963ceb1d3d9adc90
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-02-03 02:24:40 +0100

    feat: add image url to pokemon

--- src/app/services/pokemon-catalogue.service.ts
+++ src/app/services/pokemon-catalogue.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { Pokemon } from '../models/pokemon.model';
+import { Pokemon, PokemonResponse } from '../models/pokemon.model';
 import { finalize } from 'rxjs';
 
-const { API_POKEMON } = environment;
+
+
+const { API_POKEMON, API_SPRITES } = environment;
+const POKEMON_PAGE_LIMIT = 1000;
 
 @Injectable({
   providedIn: 'root',
@@ -29,21 +32,40 @@ export class PokemonCatalogueService {
   constructor(private readonly http: HttpClient) {}
 
   public findAllPokemons(): void {
+    if (this.pokemons.length > 0 || this.loading ) {
+      return;
+    }
+
     this._loading = true;
-    this.http
-    .get<{ results: Pokemon[] }>(API_POKEMON)
-    .pipe(
-      finalize(() => {
-        this._loading = false;
-      }) 
-    )
-    .subscribe({
-      next: (response) => {
-        this._pokemons = response.results;
+
+    this.http.get<PokemonResponse>(API_POKEMON + "?limit=" + POKEMON_PAGE_LIMIT).subscribe({
+      next: (response: PokemonResponse) => {
+        this._pokemons = response.results.map(
+          pokemon =>  {
+            return  {
+              ...pokemon,
+              image: this.parseImageUrl(pokemon.url),
+              id: this.parseId(pokemon.url)
+            }
+          }
+        );
       },
-      error: (error: HttpErrorResponse) => {
-        this._error=error.message;
-      },
+      error: (error: HttpErrorResponse) => { this._error = error.message },
+      complete: () => { this._loading = false },
     });
   }
+
+
+public pokemonById(pokemonId: number): Pokemon | undefined {
+  return this._pokemons.find((pokemon: Pokemon) => pokemon.id === pokemonId);
+}
+
+private parseImageUrl(url: string): string  {
+  const id = Number (url.split("/").filter(Boolean).pop());
+  return `${API_SPRITES}/${id}.png`;
+}
+
+private parseId(url: string): number {
+  return Number(url.split("/").filter(Boolean).pop());
+}
 }
