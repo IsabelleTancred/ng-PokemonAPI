81d5cdc9254ffb11b7b96d0c4ea39c7484f2ec5d
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-02-03 03:05:21 +0100

    feat: add trainer page and cought pokemon

--- src/app/pages/trainer/trainer.page.ts
+++ src/app/pages/trainer/trainer.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { User } from "../../models/user.model";
+import { UserService } from "../../services/user.service";
+import { Pokemon } from "../../models/pokemon.model";
+
+
 @Component({
   selector: 'app-trainer',
   templateUrl: './trainer.page.html',
@@ -7,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class TrainerPage {
 
+  get user(): User | undefined {
+    return this.userService.user;
+  }
+
+  get pokemon(): Pokemon[] {
+    if (this.userService.user) {
+      return this.userService.user.pokemon;
+    }
+    return [];
+  }
+
+  constructor(
+    private readonly userService: UserService,
+  ) { }
 }
