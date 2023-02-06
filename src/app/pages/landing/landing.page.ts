f72550da3350f7d0cb4c37f3a5021454c70ef9d5
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-02-06 15:04:49 +0100

    feat: add redirect on landing page

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -1,5 +1,7 @@
 import { Component} from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-landing',
@@ -8,8 +10,13 @@ import { Router } from '@angular/router';
 })
 
 export class LandingPage{
+  get user(): User | undefined {
+    return this.userService.user
+  }
 
-  constructor(private readonly router: Router) { }
+  constructor(
+    private readonly router: Router,
+    private readonly userService: UserService,) { }
  handleLogin(): void {
   this.router.navigateByUrl("/pokemon-catalogue");
  }
