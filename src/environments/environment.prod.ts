3270e034f5963e8fb560449f88a7cbb840d90ff3
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-02-06 18:55:23 +0100

    feat: update API_KEY

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   API_USER: "https://mercury-factual-bayberry.glitch.me/trainers",
   API_SPRITES: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon",
   API_POKEMON: "https://pokeapi.co/api/v2/pokemon/",
-  API_KEY: "49202471544530759446",
+  API_KEY: "eG6pRO9iob5G3I8YyttNFCC7Ns2B5XZxreFbTAKGYyiW78lMz6AMfegex4QsYgx9",
 };
